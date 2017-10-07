5d5b85e7fd832576dbf001d9171f1102feddf150
Ivan Youroff <ivan.youroff@gmail.com>
2017-10-07 16:43:02 -0400

    version bumped to push docs to hex

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule TestProbe.Mixfile do
 
   def project, do: [
     app: :test_probe,
-    version: "0.0.1",
+    version: "0.0.2",
     elixir: "~> 1.5",
     deps: deps(),
     description: description(),
